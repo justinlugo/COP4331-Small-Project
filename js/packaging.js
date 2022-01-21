2be064377f583693fd153954f05ebd22da70f722
balduin <69399383+balduin3@users.noreply.github.com>
2022-01-21 16:27:21 -0500

    added basic menu functionality

---
+++ js/packaging.js
@@ -0,0 +1,8 @@
+// We can seperate js files into "components", very similar to Java with classes.
+export const newUser = () => {
+    console.log("New user submitted.");
+}
+
+export const login = () => {
+    console.log("Login attempt.");
+}
